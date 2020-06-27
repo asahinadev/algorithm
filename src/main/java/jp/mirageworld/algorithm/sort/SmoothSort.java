f7af286b91a9fe781344e6c47a46660052dac43c
朝日奈ありす <59528169+asahinadev@users.noreply.github.com>
2020-06-27 14:52:32 +0900

    比較失敗修正

--- src/main/java/jp/mirageworld/algorithm/sort/SmoothSort.java
+++ src/main/java/jp/mirageworld/algorithm/sort/SmoothSort.java
@@ -7,8 +7,6 @@ import java.util.List;
 public class SmoothSort<E extends Comparable<? super E>>
 		implements Sort<E> {
 
-	int[] memo = { 1, 1 };
-
 	@Override
 	public List<E> apply(List<E> list) {
 
@@ -17,42 +15,48 @@ public class SmoothSort<E extends Comparable<? super E>>
 		return result;
 	}
 
-	void sort(List<E> list) {
-		if (list.isEmpty())
-			return;
+	void sort(List<E> result) {
 
-		int len = list.size();
+		if (result.size() == 0) {
+			return;
+		}
+
 		int head = 0;
 		long frac = 0;
 		int exp = 0;
 
-		while (head < len) {
+		while (head < result.size()) {
+
 			if ((frac & 3) == 3) {
 				frac >>>= 2;
 				exp += 2;
+
 			} else if (exp > 1) {
 				frac <<= exp - 1;
 				exp = 1;
+
 			} else {
 				frac <<= exp;
 				exp ^= 1;
+
 			}
+
 			frac++;
-			if (exp > 0 && len - head - 1 < leonardo(exp - 1) + 1) {
-				arrange(list, head, frac, exp);
+			if (exp > 0 && result.size() - head - 1 < leonardo(exp - 1) + 1) {
+				arrange(result, head, frac, exp);
 			}
-			sift(list, head, exp);
+			sift(result, head, exp);
 			head++;
 		}
-		arrange(list, head - 1, frac, exp);
+		arrange(result, head - 1, frac, exp);
 		while (head > 0) {
 			head--;
 			if (exp > 1) {
 				frac <<= 2;
 				frac--;
 				exp -= 2;
-				arrange(list, head - leonardo(exp) - 1, frac >> 1, exp + 1);
-				arrange(list, head - 1, frac, exp);
+				arrange(result, head - leonardo(exp) - 1, frac >> 1, exp + 1);
+				arrange(result, head - 1, frac, exp);
 			} else {
 				int trail = Long.numberOfTrailingZeros(frac - 1);
 				frac >>= trail;
@@ -60,6 +64,8 @@ public class SmoothSort<E extends Comparable<? super E>>
 			}
 		}
 	}
+
+	private static int[] memo = { 1, 1 };
 
 	int leonardo(int n) {
 		if (n < memo.length) {
@@ -70,67 +76,62 @@ public class SmoothSort<E extends Comparable<? super E>>
 			int newLength = memo.length * 2;
 			memo = Arrays.copyOf(memo, (newLength > n) ? newLength : (n + 1));
 		}
-		return memo[n] = leonardo(n - 1)
-				+ leonardo(n - 2) + 1;
+		return memo[n] = leonardo(n - 1) + leonardo(n - 2) + 1;
 	}
 
-	void sift(List<E> list, int head, int exp) {
-
-		E t = list.get(head);
+	void sift(List<E> result, int head, int exp) {
+		E t = result.get(head);
 		while (exp > 1) {
-			int li = head - 1;
-			int ri = head - 1 - leonardo(exp - 2);
+			int r = head - 1;
+			int l = head - 1 - leonardo(exp - 2);
 
-			E r = list.get(ri);
-			E l = list.get(li);
+			E left = result.get(l);
+			E right = result.get(r);
 
-			if (t.compareTo(l) >= 0 && t.compareTo(r) >= 0) {
+			if (t.compareTo(left) >= 0 && t.compareTo(right) >= 0) {
 				break;
-			} else if (l.compareTo(r) >= 0) {
-				list.set(head, l);
-				head = li;
-				exp -= 1;
-				continue;
 			}
-			list.set(head, r);
-			head = ri;
-			exp -= 2;
+			if (result.get(l).compareTo(right) >= 0) {
+				result.set(head, left);
+				head = l;
+				exp -= 1;
+			} else {
+				result.set(head, right);
+				head = r;
+				exp -= 2;
+			}
 		}
-		list.set(head, t);
+		result.set(head, t);
 	}
 
-	void arrange(List<E> list, int head, long frac, int exp) {
-		E t = list.get(head);
-
+	void arrange(List<E> v, int head, long frac, int exp) {
+		E t = v.get(head);
 		while (frac > 1) {
 			int next = head - leonardo(exp);
-			E n = list.get(next);
-
-			if (t.compareTo(list.get(next)) >= 0) {
+			if (t.compareTo(v.get(next)) >= 0) {
 				break;
 			}
 			if (exp > 1) {
-				int li = head - 1;
-				int ri = head - 1 - leonardo(exp - 2);
+				int r = head - 1;
+				int l = head - 1 - leonardo(exp - 2);
 
-				E r = list.get(ri);
-				E l = list.get(li);
+				E left = v.get(l);
+				E target = v.get(next);
+				E right = v.get(r);
 
-				if (l.compareTo(n) >= 0 || r.compareTo(n) >= 0) {
+				if (left.compareTo(target) >= 0
+						|| right.compareTo(target) >= 0) {
 					break;
 				}
 			}
 
-			list.set(head, n);
-
+			v.set(head, v.get(next));
 			head = next;
-
 			int trail = Long.numberOfTrailingZeros(frac - 1);
 			frac >>>= trail;
 			exp += trail;
 		}
-
-		list.set(head, t);
-		sift(list, head, exp);
+		v.set(head, t);
+		sift(v, head, exp);
 	}
 }
