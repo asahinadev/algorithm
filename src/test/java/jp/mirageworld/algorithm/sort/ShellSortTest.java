97178140c8538d9e2f0c51aa65fb78a5c01419f8
朝日奈ありす <59528169+asahinadev@users.noreply.github.com>
2020-06-27 11:44:03 +0900

    [未確定] シェルソート

---
+++ src/test/java/jp/mirageworld/algorithm/sort/ShellSortTest.java
@@ -0,0 +1,16 @@
+package jp.mirageworld.algorithm.sort;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class ShellSortTest extends AbstractSortTest {
+
+	@Test()
+	@SuppressWarnings("deprecation")
+	void test() {
+		// TODO ƒAƒ‹ƒSƒŠƒYƒ€’²¸’†
+		assertThrows(
+				UnsupportedOperationException.class, () -> new ShellSort().apply(list));
+	}
+}
