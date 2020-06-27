b1eb6121a7a28a0e6e495c9296befc04a5af1505
朝日奈ありす <59528169+asahinadev@users.noreply.github.com>
2020-06-27 10:58:38 +0900

    [未確定] トーナメントソート

---
+++ src/test/java/jp/mirageworld/algorithm/sort/TournamentSortTest.java
@@ -0,0 +1,16 @@
+package jp.mirageworld.algorithm.sort;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class TournamentSortTest extends AbstractSortTest {
+
+	@Test()
+	@SuppressWarnings("deprecation")
+	void test() {
+		// TODO ƒAƒ‹ƒSƒŠƒYƒ€’²¸’†
+		assertThrows(
+				UnsupportedOperationException.class, () -> new TournamentSort().apply(list));
+	}
+}
