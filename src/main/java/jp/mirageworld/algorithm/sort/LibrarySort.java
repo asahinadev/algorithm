24a5c45493e111cb64c9c2690d91d30cddad6dca
朝日奈ありす <59528169+asahinadev@users.noreply.github.com>
2020-06-27 11:46:19 +0900

    [未確定] ライブラリソート

---
+++ src/main/java/jp/mirageworld/algorithm/sort/LibrarySort.java
@@ -0,0 +1,23 @@
+package jp.mirageworld.algorithm.sort;
+
+import java.util.List;
+
+@Deprecated
+public class LibrarySort {
+
+	// TODO アルゴリズム調査中
+	public <E extends Comparable<? super E>> List<E> apply(List<E> list) {
+		throw new UnsupportedOperationException("アルゴリズム調査中");
+	}
+
+	protected <E extends Comparable<? super E>> boolean swap(List<E> result, int i, int j) {
+		E a = result.get(i);
+		E b = result.get(j);
+		if (a.compareTo(b) > 0) {
+			result.set(i, b);
+			result.set(j, a);
+			return true;
+		}
+		return false;
+	}
+}
