904d232378eeeefe73c3338c1ad1d1fffa035b33
朝日奈ありす <59528169+asahinadev@users.noreply.github.com>
2020-06-27 14:51:07 +0900

    テスト対象を 固定8件から、ランダム1000件に

--- src/test/java/jp/mirageworld/algorithm/sort/AbstractSortTest.java
+++ src/test/java/jp/mirageworld/algorithm/sort/AbstractSortTest.java
@@ -3,12 +3,15 @@ package jp.mirageworld.algorithm.sort;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class AbstractSortTest {
 
-	List<Integer> list = IntStream.of(8, 4, 3, 7, 6, 5, 2, 1).boxed().collect(Collectors.toList());
+	List<Integer> list = new Random()
+			.ints(1000, 0, 1000)
+			.boxed()
+			.collect(Collectors.toList());
 
 	public void sortTest(List<Integer> result) {
 
@@ -18,7 +21,7 @@ public class AbstractSortTest {
 			int j = i + 1;
 			int a = result.get(i);
 			int b = result.get(j);
-			assertTrue(a < b, "%d < %d".formatted(a, b));
+			assertTrue(a <= b, "%d <= %d".formatted(a, b));
 		}
 	}
 }
